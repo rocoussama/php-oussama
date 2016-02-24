50fae1f370fac80c7c0ff10ed1ccc161ed4b4f5e
Oussama <oouali@roc-dev.com>
2016-02-24 09:15:19 +0100

    test commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welkom op mijn eerste .php website</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootswatch.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+</head>
+
+<body>
+    
+    <div>
+       <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a class="navbar-brand" href="#">Home</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Github</a></li>
+                <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">php Opdrachten <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="phpOpdrachten/busreis.php" target="_blank">Bus reis</a></li>
+                    <li><a href="phpOpdrachten/kesrtboom.php" target="_blank">Kerstboom</a></li>
+                    <li><a href="phpOpdrachten/while naar fo.php" target="_blank">Loop</a></li>
+                    <li class="divider"></li>
+                    <li><a  href="phpOpdrachten/zwemmen.php" target="_blank">zwem club</a></li>
+                    <li class="divider" target="_blank"></li>
+                    <li><a href="#">Kappers zaak</a></li>
+                </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Log in</a></li>
+            </ul>
+            </div>
+        </div>
+      </nav>
+    </div>
+    
+    
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Javascript.js"></script>    
+</body>
